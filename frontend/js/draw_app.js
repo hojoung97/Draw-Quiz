5d38c7ce25eb961d2c31d760a4e5b75cb1be1302
Hojoung Jang <jang88@purdue.edu>
2021-05-30 13:29:18 -0400

    Add client socket to frontend

--- frontend/js/draw_app.js
+++ frontend/js/draw_app.js
@@ -1,19 +1,34 @@
+let socket = new WebSocket("ws://localhost:8080/ws");
+
+socket.onopen = () => {
+    console.log("Connected to Server");
+    socket.send("Hello from the client!");
+};
+
+socket.onclose = (event) => {
+    console.log("Socket Closed Connection: ", event);
+};
+
+socket.onerror = (error) => {
+    console.log("Socket Error: ", error);
+};
+
 let isDrawing = false;
 let x = 0;
 let y = 0;
 
 const myCanvas = document.getElementById("myCanvas");
 const context = myCanvas.getContext("2d");
-const brushPalette = document.getElementById("brushPalette")
-const brushSlider = document.getElementById("brushSlider")
-const clearButton = document.getElementById("clearButton")
-const submitButton = document.getElementById("submitButton")
+const brushPalette = document.getElementById("brushPalette");
+const brushSlider = document.getElementById("brushSlider");
+const clearButton = document.getElementById("clearButton");
+const submitButton = document.getElementById("submitButton");
 
 myCanvas.addEventListener("mousedown", e => {
     x = e.offsetX;
     y = e.offsetY;
     isDrawing = true;
-})
+});
 
 myCanvas.addEventListener("mousemove", e => {
     if (isDrawing === true) {
@@ -21,7 +36,7 @@ myCanvas.addEventListener("mousemove", e => {
         x = e.offsetX;
         y = e.offsetY;
     }
-})
+});
 
 window.addEventListener("mouseup", e => {
     if (isDrawing === true) {
@@ -30,7 +45,7 @@ window.addEventListener("mouseup", e => {
         y = 0;
         isDrawing = false;
     }
-})
+});
 
 function drawLine(context, x1, y1, x2, y2) {
     context.beginPath();
@@ -45,11 +60,11 @@ function drawLine(context, x1, y1, x2, y2) {
 
 
 clearButton.addEventListener("click", e => {
-    context.clearRect(0, 0, myCanvas.width, myCanvas.height)
-})
+    context.clearRect(0, 0, myCanvas.width, myCanvas.height);
+});
 
 
 submitButton.addEventListener("click", e => {
-    let drawing = context.getImageData(0, 0, myCanvas.width/2, myCanvas.height)
-    context.putImageData(drawing, myCanvas.width/2, 0)
-})
+    let drawing = context.getImageData(0, 0, myCanvas.width/2, myCanvas.height);
+    context.putImageData(drawing, myCanvas.width/2, 0);
+});
